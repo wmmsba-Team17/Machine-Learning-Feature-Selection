ea3b0c02d6b6c07e2eed502a9773a8d97dba9766
bai76 <39038227+bai76@users.noreply.github.com>
2020-01-29 10:11:35 -0500

    Update Applied Exercise 09.R

--- Applied Exercise 09.R
+++ Applied Exercise 09.R
@@ -1,8 +1,12 @@
+# Applied Exercise 09 #
+#In this exercise, we will predict the number of applications received
+#using the other variables in the College data set.
+rm(list=ls())
+
 library(glmnet)
 library(ISLR)
 library(pls)
-#a)
-rm(list=ls())
+#a)Split the data set into a training set and a test set.
 
 set.seed(1)
 indices=sample(1:nrow(College),size=0.5*nrow(College))
@@ -18,8 +22,8 @@ ytest <- test$Apps
 
 grid <-  10 ^ seq(4, -2, length=100)
 
-#c)
-
+#c)Fit a ridge regression model on the training set, with λ chosen
+#by cross-validation. Report the test error obtained.
 ridge.cv <- cv.glmnet(xtrain,ytrain,alpha=0,thresh=1e-12,lambda = grid)
 ridge.fit <- glmnet(xtrain,ytrain,alpha=0,lambda = ridge.cv$lambda.min)
 ridge.pred <- predict(ridge.fit,newx=xtest )
@@ -31,7 +35,9 @@ ridge.coef
 
 (Ridge_MSE <- mean( (ridge.pred-ytest)^2))
 
-#d)
+#d)Fit a lasso model on the training set, with λ chosen by crossvalidation.
+#Report the test error obtained, along with the number
+#of non-zero coefficient estimates.
 
 lasso.mod <- glmnet(xtrain,ytrain,alpha=1,lambda=grid)
 plot(lasso.mod)
@@ -51,7 +57,9 @@ lasso.coef <- predict(lasso2 ,type="coefficients", s=bestlam)[1:18,]
 lasso.coef
 lasso.coef[lasso.coef!=0]
 
-#e)
+#e)Fit a PCR model on the training set, with M chosen by crossvalidation.
+#Report the test error obtained, along with the value
+#of M selected by cross-validation.
 pcr.fit=pcr(Apps~.,data=train,scale=TRUE,validation='CV')
 validationplot(pcr.fit,val.type="MSEP")
 
@@ -60,7 +68,9 @@ pcr.fit$ncomp
 pcr.pred <- predict(pcr.fit,newdata = xtest,ncomp = 16)
 (PCR_MSE <- mean( (pcr.pred-ytest)^2))
 
-#f)
+#f) Fit a PLS model on the training set, with M chosen by crossvalidation.
+#Report the test error obtained, along with the value
+#of M selected by cross-validation.
 pls.fit=plsr(Apps~., data=train, scale=TRUE ,
              validation ="CV")
 
@@ -72,7 +82,7 @@ pls.pred=predict (pls.fit ,xtest,ncomp =10)
 
 (PLSMSE = mean((pls.pred -ytest)^2))
 
-#G)
+#G) Compare different methods
 
 print(c("Ridge:",Ridge_MSE))
 print(c("Lasso:",MSE_lasso))
